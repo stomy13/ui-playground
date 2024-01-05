2ea31c060f981918e8ee82c7c9e0951f8c52a079
stomy13 <masadinti@gmail.com>
2024-01-05 11:24:27 +0900

    🎨 homepage design

--- src/components/mainLayout/MainLayout.tsx
+++ src/components/mainLayout/MainLayout.tsx
@@ -1,9 +1,7 @@
-import { Box } from '@mui/material'
-
 type MainLayoutProps = {
   children: React.ReactNode
 }
 
 export const MainLayout = ({ children }: MainLayoutProps) => {
-  return <Box>{children}</Box>
+  return <>{children}</>
 }
--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom'
 import {
+  Box,
   Card,
   CardActionArea,
   CardContent,
@@ -13,24 +14,33 @@ export function HomePage() {
   const navigate = useNavigate()
   return (
     <MainLayout>
-      <Card sx={{ minWidth: 345, maxWidth: 345 }}>
-        <CardActionArea onClick={() => navigate(paths.heartLaundry)}>
-          <CardMedia
-            component="img"
-            height="140"
-            image="https://img.freepik.com/free-photo/cute-kitten-staring-out-the-window-playful-curiosity-generative-ai_188544-12520.jpg"
-            alt="cat"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Heart Laundry❤️🧺
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              心の洗濯をしよう
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+        }}
+      >
+        <Card sx={{ minWidth: 345, maxWidth: 345 }}>
+          <CardActionArea onClick={() => navigate(paths.heartLaundry)}>
+            <CardMedia
+              component="img"
+              height="140"
+              image="https://img.freepik.com/free-photo/cute-kitten-staring-out-the-window-playful-curiosity-generative-ai_188544-12520.jpg"
+              alt="cat"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Heart Laundry❤️🧺
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                心の洗濯をしよう
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Box>
     </MainLayout>
   )
 }
