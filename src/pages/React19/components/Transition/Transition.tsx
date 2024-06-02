0963ea297db1df34b76193845da659f30b6c7012
stomy13 <masadinti@gmail.com>
2024-06-02 15:15:11 +0900

    ✨ UseTransition async

--- src/pages/React19/components/Transition/Transition.tsx
+++ src/pages/React19/components/Transition/Transition.tsx
@@ -1,8 +1,17 @@
-import { Typography } from '@mui/material'
+import { useState, useTransition, useCallback } from 'react'
+import { Button, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import { EquipmentTitle } from '~/components/domain/EquipmentTitle/EquipmentTitle'
 
 export function Transition() {
+  const [count, setCount] = useState(0)
+  const [isPending, startTransition] = useTransition()
+  const handleClick = useCallback(() => {
+    startTransition(async () => {
+      await new Promise(resolve => setTimeout(resolve, 1000))
+      setCount(prev => prev + 1)
+    })
+  }, [startTransition])
   return (
     <Box
       sx={{
@@ -19,8 +28,16 @@ export function Transition() {
             whiteSpace: 'pre-wrap',
           }}
         >
-          useTransition に非同期処理を渡せるようになった
+          useTransition に非同期関数（a.k.a Actions）を渡せるようになった
         </Typography>
+        <Button variant="contained" onClick={handleClick}>
+          Increment
+        </Button>
+        {isPending ? (
+          <Typography>loading...</Typography>
+        ) : (
+          <Typography>count = {count}</Typography>
+        )}
       </Box>
     </Box>
   )
