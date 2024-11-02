799369acf45acb13baeb470cba823cb281e9e32b
stomy13 <masadinti@gmail.com>
2024-11-02 15:15:16 +0900

    refactor: rename test script and update Vitest configuration to exclude tests directory

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@
 import path from 'path'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import { configDefaults } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     includeSource: ['src/**/*.{js,ts}'],
+    exclude: [...configDefaults.exclude, 'tests'],
   },
   resolve: {
     alias: {
