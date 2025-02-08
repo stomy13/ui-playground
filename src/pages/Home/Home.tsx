2e0131661b3daa87a254df07f2e1b4feb48af8a1
stomy13 <48572568+stomy13@users.noreply.github.com>
2025-02-09 03:42:02 +0900

    feat(biome): add new linter rules for consistent curly braces and fragment syntax; update error levels fix(Home): simplify Helmet title prop syntax fix(SessionStorage): streamline description prop syntax

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,7 +8,7 @@ import { Layout } from './components/Layout/Layout'
 export function Home() {
   return (
     <MainLayout>
-      <Helmet title={'Home'} />
+      <Helmet title="Home" />
       <Layout>
         <PlaygroundCard />
         <HeartLaundryCard />
--- src/pages/Playground/components/ClientSideStorage/components/SessionStorage/SessionStorage.tsx
+++ src/pages/Playground/components/ClientSideStorage/components/SessionStorage/SessionStorage.tsx
@@ -4,9 +4,7 @@ import { WebStorage } from '../WebStorage/WebStorage'
 export function SessionStorage() {
   return (
     <WebStorage
-      description={
-        'Session Storage はタブを閉じるとデータが失われる\nページセッションが存在する間データは維持される\nページセッションはタブやブラウザーが開いている限り続き、ページの再読み込みや復元にも耐えられる'
-      }
+      description="Session Storage はタブを閉じるとデータが失われる\nページセッションが存在する間データは維持される\nページセッションはタブやブラウザーが開いている限り続き、ページの再読み込みや復元にも耐えられる"
       useStorage={useSessionStorage}
     />
   )
